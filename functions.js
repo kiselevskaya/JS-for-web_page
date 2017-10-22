d452b3c6c3a1ea20f87dc2d2d735a7d8f56376d3
kiselevskaya <kiselevskayaviktoriya@gmail.com>
2017-10-23 01:00:25 +0300

    Renamed .js and .css

---
+++ functions.js
@@ -0,0 +1,30 @@
+function color_alert(){
+    alert('clicked button with JS function')
+    var dd1 = document.getElementById("d1").style.color="#E5E513";
+    var dd2 = document.getElementById("d2");
+    var dd3 = document.getElementById("d3");
+
+    dd2.className = "morning";
+    dd3.className = "evening";
+}
+function output(message){
+  alert(message)
+}
+
+function text_confirm(){
+  var msg;
+  var choice = confirm("Press the button");
+  if (choice == true){
+    msg = "You pressed OK!";
+    output(msg);
+    var dd2 = document.getElementById("d2");
+    var dd3 = document.getElementById("d3");
+
+    dd2.innerHTML = "Bonjour";
+    dd3.innerHTML = "Sayonara";
+  }
+  else {
+    msg = "Are you sure you want to cancel";
+    output(msg);
+  }
+}
