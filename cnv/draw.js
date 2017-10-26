64bb46aa0b27daaed5a851a0faad584e887c8af0
kiselevskaya <kiselevskayaviktoriya@gmail.com>
2017-10-26 22:56:29 +0300

    Upload an image

--- cnv/draw.js
+++ cnv/draw.js
@@ -1,21 +1,6 @@
-function do_color(){
-  var dE1 = document.getElementById("div1");
-  var colorinput = document.getElementById("cc2");
-  var color = colorinput.value;
-  dE1.style.backgroundColor = color;
-}
-
-function do_square(){
-  var canvas = document.getElementById("div1");
-  var sizeinput = document.getElementById("cc1");
-  var size = sizeinput.value;
-  var context = canvas.getContext("2d");
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  context.fillStyle = "#000000";
-  context.fillRect(10,10,size,size);
-
-  var posX = parseInt(size);
-  posX += 30;
-  context.fillStyle = "#000000";
-  context.fillRect(posX,10,size,size);
+function upload(){
+  var canvas = document.getElementById("cnv");
+  var finput = document.getElementById("finput");
+  var img = new SimpleImage(finput);
+  img.drawTo(canvas);
 }
