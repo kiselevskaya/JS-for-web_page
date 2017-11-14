685c3cc7580f3531c4aa1d8d1b84435b69f7f1fd
kiselevskaya <kiselevskayaviktoriya@gmail.com>
2017-11-14 16:36:10 +0300

    Create Composition with green screen

--- cnv/draw.js
+++ cnv/draw.js
@@ -1,31 +1,51 @@
-function orange_color(){
-  var dE1 = document.getElementById("div1");
-  dE1.style.backgroundColor = "#FF4000";
+var frontimg=null;
+var backimg=null;
+var fcnv;
+var scnv;
 
-  var context = dE1.getContext("2d");
-  context.fillStyle = "#FF6347";
-  //context.fillRect(10,10,60,60);
-  //context.fillRect(80,10,60,60);
-
-  context.beginPath();
-  context.arc(90,75,60,0,2*Math.PI);
-  context.stroke();
-
-  context.beginPath();
-  context.arc(200,75,50,0,2*Math.PI);
-  context.stroke();
-
-  context.fillStyle = "#0F0000";
-  context.font = "30px Arial";
-  context.fillText("@",90,90);
-  context.fillText("@",200,90);
+function loadForegroundImage(){
+  fcnv = document.getElementById("cnv");
+  var fimg = document.getElementById("finput");
+  frontimg = new SimpleImage(fimg);
+  frontimg.drawTo(fcnv);
+  alert("foreground image loaded");
 }
-function green_color(){
-  var dE2 = document.getElementById("div2");
-  dE2.style.backgroundColor = "#1FD41F";
 
-  var canvas = document.getElementById("div1");
-  var context = canvas.getContext("2d");
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  canvas.style.backgroundColor = "#D4D41F";
+function loadBackgroundImage(){
+  scnv = document.getElementById("2cnv");
+  var simg = document.getElementById("2input");
+  backimg = new SimpleImage(simg);
+  backimg.drawTo(scnv);
+  alert("background image loaded");
+}
+
+function compose(){
+  if (frontimg==null || ! frontimg.complete()){
+    alert("foreground not loaded");
+    return;
+  }
+  if (backimg==null || ! backimg.complete()){
+    alert("background not loaded");
+  }
+  var newimg = new SimpleImage(frontimg.getWidth(),frontimg.getHeight());
+  for (var pixel of frontimg.values()){
+      var x = pixel.getX();
+      var y = pixel.getY();
+    if (pixel.getGreen()>pixel.getRed()+pixel.getBlue()){
+      var bgpixel=backimg.getPixel(x,y);
+      newimg.setPixel(x,y,bgpixel);
+    }
+    else{
+      newimg.setPixel(x,y,pixel);
+    }
+  }
+  clearCnvs();
+  newimg.drawTo(fcnv);
+}
+
+function clearCnvs(){
+  var context = fcnv.getContext("2d");
+  var ctx = scnv.getContext("2d");
+  context.clearRect(0, 0, fcnv.width, fcnv.height);
+  ctx.clearRect(0, 0, scnv.width, scnv.height);
 }
